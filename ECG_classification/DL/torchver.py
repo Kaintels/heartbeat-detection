eda7dfd05bc7cfb1c460daf5ab49c1bbc4312a8d
Han <swoohan@outlook.kr>
2020-05-13 08:57:27 +0900

    Update torchver.py

--- ECG_classification/DL/torchver.py
+++ ECG_classification/DL/torchver.py
@@ -3,88 +3,217 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import scipy
+# x = pd.read_csv(r"C:\Users\3ttook\Documents\Source\Python\ERC\ECG-classification\src\feature_save\feature_s\OUR\DS1.csv")
+# y = pd.read_csv(r"C:\Users\3ttook\Documents\Source\Python\ERC\ECG-classification\src\feature_save\feature_s\OUR\DS2.csv")
+#
+# np.random.shuffle(x.values)
+# np.random.shuffle(y.values)
+#
+# x_train = x.values[:,:1272]
+# x_test = x[:, 1272]
+
+import time
+
+start = time.time()  # 시작 시간 저장
+
+# ml channel
+x_train = pd.read_csv('../arrhythmia/ds1_ml.csv')
+x_test = pd.read_csv('../arrhythmia/ds2_ml.csv')
+
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+
+# scipy.io.savemat(r'C:\Users\3ttook\Documents\Source\Python\ERC\ECG-classification\REAL-T~1\data\1.mat', {'x_train' : x_train})
+
+
+# x_train_st = []
+# for i in range(len(x_train)-1):
+#     data = np.hstack((x_train[i], x_train[i+1]))
+#     x_train_st.append(data)
+
+# x_train_st = np.array(x_train_st)
+
+# x_test_st = []
+# for i in range(len(x_test)-1):
+#     data = np.hstack((x_test[i], x_test[i+1]))
+#     x_test_st.append(data)
+
+# x_test_st = np.array(x_test_st)
+
+
+x_train_st = []
+for i in range(len(x_train) - 2):
+    data = np.hstack((x_train[i], x_train[i + 1], x_train[i + 2]))
+    x_train_st.append(data)
+
+x_train_st = np.array(x_train_st)
+
+x_test_st = []
+for i in range(len(x_test) - 2):
+    data = np.hstack((x_test[i], x_test[i + 1], x_test[i + 2]))
+    x_test_st.append(data)
+
+x_test_st = np.array(x_test_st)
+
+# # # # v channel
+# # # # t_ds1 = pd.read_csv(r'C:\Users\3ttook\Documents\Source\Python\ERC\ECG-classification\src\feature_save\signal\170\ds1_v.csv')
+# # # # t_ds2 = pd.read_csv(r'C:\Users\3ttook\Documents\Source\Python\ERC\ECG-classification\src\feature_save\signal\170\ds2_v.csv')
+
+# # # # x_train = np.dstack((o_ds1, t_ds1))
+# # # # x_test = np.dstack((o_ds2, t_ds2))
+
+x_train = x_train_st[:, :, np.newaxis]
+x_test = x_test_st[:, :, np.newaxis]
+#
+
+# x_train = np.expand_dims(x_train, axis=2)
+# x_test = np.expand_dims(x_test, axis=2)
+
+
+# # # # label
+y_train = pd.read_csv('../arrhythmia/ds1_onehot.csv')
+y_test = pd.read_csv('../arrhythmia//ds2_onehot.csv')
+
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+y_train_st = []
+for i in range(len(y_train) - 2):
+    data = np.hstack((y_train[i], y_train[i + 1], y_train[i + 2]))
+    y_train_st.append(data)
+
+y_train_st = np.array(y_train_st)
+
+y_test_st = []
+for i in range(len(y_test) - 2):
+    data = np.hstack((y_test[i], y_test[i + 1], y_test[i + 2]))
+    y_test_st.append(data)
+
+y_test_st = np.array(y_test_st)
+
+# # y_train_st = []
+# # for i in range(len(y_train)):
+# #     data = np.hstack((y_train[i]))
+# #     y_train_st.append(data)
+
+# # y_train_st = np.array(y_train_st)
+
+# # y_test_st = []
+# # for i in range(len(y_test)):
+# #     data = np.hstack((y_test[i]))
+# #     y_test_st.append(data)
+
+# # y_test_st = np.array(y_test_st)
+
+
+for i in range(len(y_test_st)):
+    if sum(y_test_st[i]) == 0:
+        y_test_st[i] = [0]
+    else:
+        y_test_st[i] = [1]
+
+for i in range(len(y_train_st)):
+    if sum(y_train_st[i]) == 0:
+        y_train_st[i] = [0]
+    else:
+        y_train_st[i] = [1]
+
+y_train = y_train_st[0:len(y_train_st), 0:1]
+y_test = y_test_st[0:len(y_test_st), 0:1]
+
+# from sklearn.preprocessing import OneHotEncoder
+
+# onehot_encoder = OneHotEncoder()
+
+# y_train = onehot_encoder.fit_transform(y_train).toarray()
+# y_test = onehot_encoder.fit_transform(y_test).toarray()
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
-x_train = torch.from_numpy(x_train).float().permute(0,2,1)
-x_test = torch.from_numpy(x_test).float().permute(0,2,1)
+x_train = torch.from_numpy(x_train).float().permute(0, 2, 1)
+x_test = torch.from_numpy(x_test).float().permute(0, 2, 1)
 
 y_train = torch.from_numpy(y_train).long()
 y_test = torch.from_numpy(y_test).long()
 
-train = TensorDataset(x_train, y_train)
-train_loader = DataLoader(train, batch_size=16, shuffle=True)
-test = TensorDataset(x_test, y_test)
-test_loader = DataLoader(test, batch_size=16, shuffle=True)
+batch_sizes = 16
 
-class Net(nn.Module):
+train = TensorDataset(x_train, y_train)
+train_loader = DataLoader(train, batch_size=batch_sizes, shuffle=True)
+test = TensorDataset(x_test, y_test)
+test_loader = DataLoader(test, batch_size=batch_sizes, shuffle=True)
+
+
+class DetectionNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(DetectionNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(32)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(32),
             nn.MaxPool1d(2),
         )
         self.layer3 = nn.Sequential(
-            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(32),
             nn.MaxPool1d(2),
         )
         self.layer4 = nn.Sequential(
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(64),
             nn.MaxPool1d(2),
         )
         self.layer5 = nn.Sequential(
-            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(64),
             nn.MaxPool1d(2)
         )
         self.layer6 = nn.Sequential(
-            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(128),
             nn.MaxPool1d(2)
         )
         self.layer7 = nn.Sequential(
-            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(128),
             nn.MaxPool1d(2)
         )
         self.layer8 = nn.Sequential(
-            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(256),
             nn.MaxPool1d(2)
         )
         self.layer9 = nn.Sequential(
-            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3, padding_mode='same', padding=1),
+            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3, padding_mode='zeros', padding=1),
             nn.ReLU(),
             nn.BatchNorm1d(256),
         )
         self.layer10 = nn.Sequential(
-            nn.LSTM(3,64,1)
+            nn.LSTM(3, 64, 1)
         )
         self.flatten = nn.Flatten()
         self.fc1 = nn.Sequential(
             nn.Linear(16384, 2),
             nn.LogSoftmax()
         )
-    def forward(self, x):
-        out = self.layer1(x)
+
+    def forward(self, out):
+        out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
@@ -99,12 +228,9 @@ class Net(nn.Module):
         return out
 
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') #if torch.cuda.is_available() else 'cpu'
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # if torch.cuda.is_available() else 'cpu'
 
-
-
-
-model = Net().to(device)
+model = DetectionNet().to(device)
 num_epochs = 1
 from torchsummaryX import summary
 
@@ -115,7 +241,6 @@ print(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (x_train, y_train) in enumerate(train_loader):
@@ -124,16 +249,15 @@ for epoch in range(num_epochs):
         # Forward pass
         outputs = model(x_train)
         loss = criterion(outputs, y_train.squeeze())
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
+        if (i + 1) % batch_sizes == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
 from sklearn.metrics import accuracy_score
 
@@ -145,11 +269,11 @@ with torch.no_grad():
     for x_test, y_test in test_loader:
         x_test = x_test.to(device)
         y_test = y_test.to(device)
-        
+
         outputs = model(x_test)
         _, predicted = torch.max(outputs.data, 1)
         total += y_test.size(0)
         correct += (predicted == y_test.squeeze()).sum().item()
 
     print('Test Accuracy of the model: {} %'.format(100 * correct / total))
-    print("time :", time.time() - start)
+    print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
